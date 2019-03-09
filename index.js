bcca2bc34fca74becbddf724c566afc4bb2803ac
Madhuri Chalamangalam <madhuri.chalamangalam@gmail.com>
2019-03-09 03:45:15 +0000

    final solution

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+
+
 function sumItems(array) {
+  var sum =0;
   // Sum all the numbers in the array
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      sum += sumItems(item);
+      
+    } else {
+      sum += item;
+      
+    }
+    
+});
+return sum;
 }
 
 module.exports = sumItems;
